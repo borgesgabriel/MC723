a1a15ba8b1f64fee185690dd2bf7007c3b0a90e5
Ulisses Malta Santos <ulissesmaltasantos@gmail.com>
2015-04-17 11:42:53 -0300

    Implement instruction buffer, count instructions, print instruction buffer, delete nop operation

--- mips/mips_isa.cpp
+++ mips/mips_isa.cpp
@@ -13,9 +13,9 @@
  *
  * @version   1.0
  * @date      Mon, 19 Jun 2006 15:50:52 -0300
- * 
+ *
  * @brief     The ArchC i8051 functional model.
- * 
+ *
  * @attention Copyright (C) 2002-2006 --- The ArchC Team
  *
  */
@@ -24,9 +24,10 @@
 #include  "mips_isa_init.cpp"
 #include  "mips_bhv_macros.H"
 
+#include <deque>
 
 //If you want debug information for this model, uncomment next line
-//#define DEBUG_MODEL
+#define DEBUG_MODEL
 #include "ac_debug_model.H"
 
 
@@ -41,22 +42,141 @@ using namespace mips_parms;
 static int processors_started = 0;
 #define DEFAULT_STACK_SIZE (256*1024)
 
+// *****************************************************
+
+struct mips_instruction
+{
+  enum i_type {kR, kI, kJ};
+  i_type type;
+  unsigned int op;
+  unsigned int rs;
+  unsigned int rt;
+  unsigned int rd;
+  unsigned int shamt;
+  unsigned int func;
+  unsigned int addr;
+  int imm;
+
+};
+
+std::ostream& operator<<(std::ostream& os, const mips_instruction& inst)
+{
+  switch (inst.type)
+  {
+  case mips_instruction::kR:
+    os << "R-instruction: " <<
+       inst.op << " " <<
+       inst.rs << ", " <<
+       inst.rt << ", " <<
+       inst.rd << ", " <<
+       inst.shamt << ", " <<
+       inst.func;
+    break;
+  case mips_instruction::kI:
+    os << "I-instruction: " <<
+       inst.op << " " <<
+       inst.rs << ", " <<
+       inst.rt << ", " <<
+       inst.imm;
+    break;
+  case mips_instruction::kJ:
+    os << "J-instruction: " <<
+       inst.op << " " <<
+       inst.addr;
+    break;
+  default:
+    os << "Invalid instruction!";
+    break;
+  }
+  return os;
+}
+
+struct variables
+{
+  int number_of_instructions;
+  static constexpr int kNumberOfStoredInstructions = 10;
+  std::deque<mips_instruction> latest_instructions;
+
+  variables()
+  {
+    number_of_instructions = 0;
+  }
+
+  void push(mips_instruction inst)
+  {
+    std::cout << inst << std::endl;
+    latest_instructions.push_front(inst);
+    if (latest_instructions.size() > kNumberOfStoredInstructions)
+    {
+      latest_instructions.pop_back();
+    }
+  }
+} global;
+
+// *****************************************************
+
 //!Generic instruction behavior method.
 void ac_behavior( instruction )
-{ 
+{
+  global.number_of_instructions++;
+
   dbg_printf("----- PC=%#x ----- %lld\n", (int) ac_pc, ac_instr_counter);
   //  dbg_printf("----- PC=%#x NPC=%#x ----- %lld\n", (int) ac_pc, (int)npc, ac_instr_counter);
 #ifndef NO_NEED_PC_UPDATE
   ac_pc = npc;
   npc = ac_pc + 4;
-#endif 
+#endif
 };
- 
+
 //! Instruction Format behavior methods.
-void ac_behavior( Type_R ){}
-void ac_behavior( Type_I ){}
-void ac_behavior( Type_J ){}
- 
+void ac_behavior( Type_R )
+{
+  global.push(
+  {
+    mips_instruction::kR,
+    op,
+    rs,
+    rt,
+    rd,
+    shamt,
+    func,
+    0,
+    0
+  });
+}
+
+void ac_behavior( Type_I )
+{
+  global.push(
+  {
+    mips_instruction::kI,
+    op,
+    rs,
+    rt,
+    0,
+    0,
+    0,
+    0,
+    imm
+  });
+}
+
+void ac_behavior( Type_J )
+{
+  global.push(
+  {
+    mips_instruction::kJ,
+    op,
+    0,
+    0,
+    0,
+    0,
+    0,
+    addr,
+    0
+  });
+}
+
 //!Behavior called before starting simulation
 void ac_behavior(begin)
 {
@@ -71,14 +191,16 @@ void ac_behavior(begin)
   lo = 0;
 
   RB[29] =  AC_RAM_END - 1024 - processors_started++ * DEFAULT_STACK_SIZE;
-
-
 }
 
 //!Behavior called after finishing simulation
 void ac_behavior(end)
 {
   dbg_printf("@@@ end behavior @@@\n");
+
+  printf("******************************\n");
+  printf("Number of Instructions: %d\n", global.number_of_instructions);
+  printf("******************************\n");
 }
 
 
@@ -87,14 +209,14 @@ void ac_behavior( lb )
 {
   char byte;
   unsigned address, offset;
-  
+
   dbg_printf("lb r%d, %d(r%d)\n", rt, imm & 0xFFFF, rs);
-  
+
   address = RB[rs] + imm;
   offset = address & 3;
   byte = (DM.read(address & ~3) >> ((3 - offset) * 8)) & 0xFF;
   RB[rt] = (ac_Sword)byte ;
-  
+
   dbg_printf("Result = %#x\n", RB[rt]);
 };
 
@@ -103,12 +225,12 @@ void ac_behavior( lbu )
 {
   unsigned char byte;
   unsigned address, offset;
-  
+
   dbg_printf("lbu r%d, %d(r%d)\n", rt, imm & 0xFFFF, rs);
   address = RB[rs] + imm;
   offset = address & 3;
   byte = (DM.read(address & ~3) >> ((3 - offset) * 8)) & 0xFF;
-  
+
   RB[rt] = byte;
   dbg_printf("Result = %#x\n", RB[rt]);
 };
@@ -118,12 +240,12 @@ void ac_behavior( lh )
 {
   short int half;
   unsigned address, offset;
-  
+
   dbg_printf("lh r%d, %d(r%d)\n", rt, imm & 0xFFFF, rs);
-  address = RB[rs]+ imm;
+  address = RB[rs] + imm;
   offset = (address & 3) >> 1;
   half = (DM.read(address & ~3) >> (1 - offset) * 16) & 0xFFFF;
-  
+
   RB[rt] = (ac_Sword) half;
   dbg_printf("Result = %#x\n", RB[rt]);
 };
@@ -135,10 +257,10 @@ void ac_behavior( lhu )
   unsigned address, offset;
 
   dbg_printf("lhu r%d, %d(r%d)\n", rt, imm & 0xFFFF, rs);
-  address = RB[rs]+ imm;
+  address = RB[rs] + imm;
   offset = (address & 3) >> 1;
   half = (DM.read(address & ~3) >> (1 - offset) * 16) & 0xFFFF;
-  
+
   RB[rt] = half;
   dbg_printf("Result = %#x\n", RB[rt]);
 };
@@ -147,7 +269,7 @@ void ac_behavior( lhu )
 void ac_behavior( lw )
 {
   dbg_printf("lw r%d, %d(r%d)\n", rt, imm & 0xFFFF, rs);
-  RB[rt] = DM.read(RB[rs]+ imm);
+  RB[rt] = DM.read(RB[rs] + imm);
   dbg_printf("Result = %#x\n", RB[rt]);
 };
 
@@ -162,7 +284,7 @@ void ac_behavior( lwl )
   offset = (addr & 0x3) * 8;
   data = DM.read(addr & 0xFFFFFFFC);
   data <<= offset;
-  data |= RB[rt] & ((1<<offset)-1);
+  data |= RB[rt] & ((1 << offset) - 1);
   RB[rt] = data;
   dbg_printf("Result = %#x\n", RB[rt]);
 };
@@ -178,7 +300,7 @@ void ac_behavior( lwr )
   offset = (3 - (addr & 0x3)) * 8;
   data = DM.read(addr & 0xFFFFFFFC);
   data >>= offset;
-  data |= RB[rt] & (0xFFFFFFFF << (32-offset));
+  data |= RB[rt] & (0xFFFFFFFF << (32 - offset));
   RB[rt] = data;
   dbg_printf("Result = %#x\n", RB[rt]);
 };
@@ -189,15 +311,15 @@ void ac_behavior( sb )
   unsigned char byte;
   unsigned address, offset_ammount;
   ac_word data;
-  
+
   dbg_printf("sb r%d, %d(r%d)\n", rt, imm & 0xFFFF, rs);
-  
+
   address = RB[rs] + imm;
   offset_ammount = (3 - (address & 3)) * 8;
   byte = RB[rt] & 0xFF;
-  data = DM.read(address & ~3) & ~(0xFF << offset_ammount) | (byte << offset_ammount); 
+  data = DM.read(address & ~3) & ~(0xFF << offset_ammount) | (byte << offset_ammount);
   DM.write(address & ~3, data);
-  
+
   dbg_printf("Result = %#x\n", (int) byte);
 };
 
@@ -207,15 +329,15 @@ void ac_behavior( sh )
   unsigned short int half;
   unsigned address, offset_ammount;
   ac_word data;
-  
+
   dbg_printf("sh r%d, %d(r%d)\n", rt, imm & 0xFFFF, rs);
-  
+
   address = RB[rs] + imm;
   offset_ammount = (1 - ((address & 3) >> 1)) * 16;
   half = RB[rt] & 0xFFFF;
   data = DM.read(address & ~3) & ~(0xFFFF << offset_ammount) | (half << offset_ammount);
   DM.write(address & ~3, data);
-  
+
   dbg_printf("Result = %#x\n", (int) half);
 };
 
@@ -238,7 +360,7 @@ void ac_behavior( swl )
   offset = (addr & 0x3) * 8;
   data = RB[rt];
   data >>= offset;
-  data |= DM.read(addr & 0xFFFFFFFC) & (0xFFFFFFFF << (32-offset));
+  data |= DM.read(addr & 0xFFFFFFFC) & (0xFFFFFFFF << (32 - offset));
   DM.write(addr & 0xFFFFFFFC, data);
   dbg_printf("Result = %#x\n", data);
 };
@@ -254,7 +376,7 @@ void ac_behavior( swr )
   offset = (3 - (addr & 0x3)) * 8;
   data = RB[rt];
   data <<= offset;
-  data |= DM.read(addr & 0xFFFFFFFC) & ((1<<offset)-1);
+  data |= DM.read(addr & 0xFFFFFFFC) & ((1 << offset) - 1);
   DM.write(addr & 0xFFFFFFFC, data);
   dbg_printf("Result = %#x\n", data);
 };
@@ -267,8 +389,10 @@ void ac_behavior( addi )
   dbg_printf("Result = %#x\n", RB[rt]);
   //Test overflow
   if ( ((RB[rs] & 0x80000000) == (imm & 0x80000000)) &&
-       ((imm & 0x80000000) != (RB[rt] & 0x80000000)) ) {
-    fprintf(stderr, "EXCEPTION(addi): integer overflow.\n"); exit(EXIT_FAILURE);
+       ((imm & 0x80000000) != (RB[rt] & 0x80000000)) )
+  {
+    fprintf(stderr, "EXCEPTION(addi): integer overflow.\n");
+    exit(EXIT_FAILURE);
   }
 };
 
@@ -285,7 +409,7 @@ void ac_behavior( slti )
 {
   dbg_printf("slti r%d, r%d, %d\n", rt, rs, imm & 0xFFFF);
   // Set the RD if RS< IMM
-  if( (ac_Sword) RB[rs] < (ac_Sword) imm )
+  if ( (ac_Sword) RB[rs] < (ac_Sword) imm )
     RB[rt] = 1;
   // Else reset RD
   else
@@ -298,7 +422,7 @@ void ac_behavior( sltiu )
 {
   dbg_printf("sltiu r%d, r%d, %d\n", rt, rs, imm & 0xFFFF);
   // Set the RD if RS< IMM
-  if( (ac_Uword) RB[rs] < (ac_Uword) imm )
+  if ( (ac_Uword) RB[rs] < (ac_Uword) imm )
     RB[rt] = 1;
   // Else reset RD
   else
@@ -308,7 +432,7 @@ void ac_behavior( sltiu )
 
 //!Instruction andi behavior method.
 void ac_behavior( andi )
-{	
+{
   dbg_printf("andi r%d, r%d, %d\n", rt, rs, imm & 0xFFFF);
   RB[rt] = RB[rs] & (imm & 0xFFFF) ;
   dbg_printf("Result = %#x\n", RB[rt]);
@@ -316,7 +440,7 @@ void ac_behavior( andi )
 
 //!Instruction ori behavior method.
 void ac_behavior( ori )
-{	
+{
   dbg_printf("ori r%d, r%d, %d\n", rt, rs, imm & 0xFFFF);
   RB[rt] = RB[rs] | (imm & 0xFFFF) ;
   dbg_printf("Result = %#x\n", RB[rt]);
@@ -324,7 +448,7 @@ void ac_behavior( ori )
 
 //!Instruction xori behavior method.
 void ac_behavior( xori )
-{	
+{
   dbg_printf("xori r%d, r%d, %d\n", rt, rs, imm & 0xFFFF);
   RB[rt] = RB[rs] ^ (imm & 0xFFFF) ;
   dbg_printf("Result = %#x\n", RB[rt]);
@@ -332,7 +456,7 @@ void ac_behavior( xori )
 
 //!Instruction lui behavior method.
 void ac_behavior( lui )
-{	
+{
   dbg_printf("lui r%d, r%d, %d\n", rt, rs, imm & 0xFFFF);
   // Load a constant in the upper 16 bits of a register
   // To achieve the desired behaviour, the constant was shifted 16 bits left
@@ -349,8 +473,10 @@ void ac_behavior( add )
   dbg_printf("Result = %#x\n", RB[rd]);
   //Test overflow
   if ( ((RB[rs] & 0x80000000) == (RB[rd] & 0x80000000)) &&
-       ((RB[rd] & 0x80000000) != (RB[rt] & 0x80000000)) ) {
-    fprintf(stderr, "EXCEPTION(add): integer overflow.\n"); exit(EXIT_FAILURE);
+       ((RB[rd] & 0x80000000) != (RB[rt] & 0x80000000)) )
+  {
+    fprintf(stderr, "EXCEPTION(add): integer overflow.\n");
+    exit(EXIT_FAILURE);
   }
 };
 
@@ -383,10 +509,10 @@ void ac_behavior( subu )
 
 //!Instruction slt behavior method.
 void ac_behavior( slt )
-{	
+{
   dbg_printf("slt r%d, r%d, r%d\n", rd, rs, rt);
   // Set the RD if RS< RT
-  if( (ac_Sword) RB[rs] < (ac_Sword) RB[rt] )
+  if ( (ac_Sword) RB[rs] < (ac_Sword) RB[rt] )
     RB[rd] = 1;
   // Else reset RD
   else
@@ -399,7 +525,7 @@ void ac_behavior( sltu )
 {
   dbg_printf("sltu r%d, r%d, r%d\n", rd, rs, rt);
   // Set the RD if RS < RT
-  if( RB[rs] < RB[rt] )
+  if ( RB[rs] < RB[rt] )
     RB[rd] = 1;
   // Else reset RD
   else
@@ -440,14 +566,14 @@ void ac_behavior( instr_nor )
 };
 
 //!Instruction nop behavior method.
-void ac_behavior( nop )
-{  
-  dbg_printf("nop\n");
-};
+//void ac_behavior( nop )
+//{
+//  dbg_printf("nop\n");
+//};
 
 //!Instruction sll behavior method.
 void ac_behavior( sll )
-{  
+{
   dbg_printf("sll r%d, r%d, %d\n", rd, rs, shamt);
   RB[rd] = RB[rt] << shamt;
   dbg_printf("Result = %#x\n", RB[rd]);
@@ -530,7 +656,7 @@ void ac_behavior( multu )
   // Register LO receives 32 less significant bits
   lo = half_result;
 
-  half_result = ((result>>32) & 0xFFFFFFFF);
+  half_result = ((result >> 32) & 0xFFFFFFFF);
   // Register HI receives 32 most significant bits
   hi = half_result ;
 
@@ -596,7 +722,7 @@ void ac_behavior( j )
   addr = addr << 2;
 #ifndef NO_NEED_PC_UPDATE
   npc =  (ac_pc & 0xF0000000) | addr;
-#endif 
+#endif
   dbg_printf("Target = %#x\n", (ac_pc & 0xF0000000) | addr );
 };
 
@@ -607,15 +733,15 @@ void ac_behavior( jal )
   // Save the value of PC + 8 (return address) in $ra ($31) and
   // jump to the address given by PC(31...28)||(addr<<2)
   // It must also flush the instructions that were loaded into the pipeline
-  RB[Ra] = ac_pc+4; //ac_pc is pc+4, we need pc+8
-	
+  RB[Ra] = ac_pc + 4; //ac_pc is pc+4, we need pc+8
+
   addr = addr << 2;
 #ifndef NO_NEED_PC_UPDATE
   npc = (ac_pc & 0xF0000000) | addr;
-#endif 
-	
+#endif
+
   dbg_printf("Target = %#x\n", (ac_pc & 0xF0000000) | addr );
-  dbg_printf("Return = %#x\n", ac_pc+4);
+  dbg_printf("Return = %#x\n", ac_pc + 4);
 };
 
 //!Instruction jr behavior method.
@@ -626,7 +752,7 @@ void ac_behavior( jr )
   // It must also flush the instructions that were loaded into the pipeline
 #ifndef NO_NEED_PC_UPDATE
   npc = RB[rs], 1;
-#endif 
+#endif
   dbg_printf("Target = %#x\n", RB[rs]);
 };
 
@@ -639,113 +765,121 @@ void ac_behavior( jalr )
 
 #ifndef NO_NEED_PC_UPDATE
   npc = RB[rs], 1;
-#endif 
+#endif
   dbg_printf("Target = %#x\n", RB[rs]);
 
-  if( rd == 0 )  //If rd is not defined use default
+  if ( rd == 0 ) //If rd is not defined use default
     rd = Ra;
-  RB[rd] = ac_pc+4;
-  dbg_printf("Return = %#x\n", ac_pc+4);
+  RB[rd] = ac_pc + 4;
+  dbg_printf("Return = %#x\n", ac_pc + 4);
 };
 
 //!Instruction beq behavior method.
 void ac_behavior( beq )
 {
   dbg_printf("beq r%d, r%d, %d\n", rt, rs, imm & 0xFFFF);
-  if( RB[rs] == RB[rt] ){
+  if ( RB[rs] == RB[rt] )
+  {
 #ifndef NO_NEED_PC_UPDATE
-    npc = ac_pc + (imm<<2);
-#endif 
-    dbg_printf("Taken to %#x\n", ac_pc + (imm<<2));
-  }	
+    npc = ac_pc + (imm << 2);
+#endif
+    dbg_printf("Taken to %#x\n", ac_pc + (imm << 2));
+  }
 };
 
 //!Instruction bne behavior method.
 void ac_behavior( bne )
-{	
+{
   dbg_printf("bne r%d, r%d, %d\n", rt, rs, imm & 0xFFFF);
-  if( RB[rs] != RB[rt] ){
+  if ( RB[rs] != RB[rt] )
+  {
 #ifndef NO_NEED_PC_UPDATE
-    npc = ac_pc + (imm<<2);
-#endif 
-    dbg_printf("Taken to %#x\n", ac_pc + (imm<<2));
-  }	
+    npc = ac_pc + (imm << 2);
+#endif
+    dbg_printf("Taken to %#x\n", ac_pc + (imm << 2));
+  }
 };
 
 //!Instruction blez behavior method.
 void ac_behavior( blez )
 {
   dbg_printf("blez r%d, %d\n", rs, imm & 0xFFFF);
-  if( (RB[rs] == 0 ) || (RB[rs]&0x80000000 ) ){
+  if ( (RB[rs] == 0 ) || (RB[rs] & 0x80000000 ) )
+  {
 #ifndef NO_NEED_PC_UPDATE
-    npc = ac_pc + (imm<<2), 1;
-#endif 
-    dbg_printf("Taken to %#x\n", ac_pc + (imm<<2));
-  }	
+    npc = ac_pc + (imm << 2), 1;
+#endif
+    dbg_printf("Taken to %#x\n", ac_pc + (imm << 2));
+  }
 };
 
 //!Instruction bgtz behavior method.
 void ac_behavior( bgtz )
 {
   dbg_printf("bgtz r%d, %d\n", rs, imm & 0xFFFF);
-  if( !(RB[rs] & 0x80000000) && (RB[rs]!=0) ){
+  if ( !(RB[rs] & 0x80000000) && (RB[rs] != 0) )
+  {
 #ifndef NO_NEED_PC_UPDATE
-    npc = ac_pc + (imm<<2);
-#endif 
-    dbg_printf("Taken to %#x\n", ac_pc + (imm<<2));
-  }	
+    npc = ac_pc + (imm << 2);
+#endif
+    dbg_printf("Taken to %#x\n", ac_pc + (imm << 2));
+  }
 };
 
 //!Instruction bltz behavior method.
 void ac_behavior( bltz )
 {
   dbg_printf("bltz r%d, %d\n", rs, imm & 0xFFFF);
-  if( RB[rs] & 0x80000000 ){
+  if ( RB[rs] & 0x80000000 )
+  {
 #ifndef NO_NEED_PC_UPDATE
-    npc = ac_pc + (imm<<2);
-#endif 
-    dbg_printf("Taken to %#x\n", ac_pc + (imm<<2));
-  }	
+    npc = ac_pc + (imm << 2);
+#endif
+    dbg_printf("Taken to %#x\n", ac_pc + (imm << 2));
+  }
 };
 
 //!Instruction bgez behavior method.
 void ac_behavior( bgez )
 {
   dbg_printf("bgez r%d, %d\n", rs, imm & 0xFFFF);
-  if( !(RB[rs] & 0x80000000) ){
+  if ( !(RB[rs] & 0x80000000) )
+  {
 #ifndef NO_NEED_PC_UPDATE
-    npc = ac_pc + (imm<<2);
-#endif 
-    dbg_printf("Taken to %#x\n", ac_pc + (imm<<2));
-  }	
+    npc = ac_pc + (imm << 2);
+#endif
+    dbg_printf("Taken to %#x\n", ac_pc + (imm << 2));
+  }
 };
 
 //!Instruction bltzal behavior method.
 void ac_behavior( bltzal )
 {
   dbg_printf("bltzal r%d, %d\n", rs, imm & 0xFFFF);
-  RB[Ra] = ac_pc+4; //ac_pc is pc+4, we need pc+8
-  if( RB[rs] & 0x80000000 ){
+  RB[Ra] = ac_pc + 4; //ac_pc is pc+4, we need pc+8
+  if ( RB[rs] & 0x80000000 )
+  {
 #ifndef NO_NEED_PC_UPDATE
-    npc = ac_pc + (imm<<2);
-#endif 
-    dbg_printf("Taken to %#x\n", ac_pc + (imm<<2));
-  }	
-  dbg_printf("Return = %#x\n", ac_pc+4);
+    npc = ac_pc + (imm << 2);
+#endif
+    dbg_printf("Taken to %#x\n", ac_pc + (imm << 2));
+  }
+  dbg_printf("Return = %#x\n", ac_pc + 4);
 };
 
 //!Instruction bgezal behavior method.
 void ac_behavior( bgezal )
 {
   dbg_printf("bgezal r%d, %d\n", rs, imm & 0xFFFF);
-  RB[Ra] = ac_pc+4; //ac_pc is pc+4, we need pc+8
-  if( !(RB[rs] & 0x80000000) ){
+  RB[Ra] = ac_pc + 4; //ac_pc is pc+4, we need pc+8
+  if ( !(RB[rs] & 0x80000000) )
+  {
 #ifndef NO_NEED_PC_UPDATE
-    npc = ac_pc + (imm<<2);
-#endif 
-    dbg_printf("Taken to %#x\n", ac_pc + (imm<<2));
-  }	
-  dbg_printf("Return = %#x\n", ac_pc+4);
+    npc = ac_pc + (imm << 2);
+#endif
+    dbg_printf("Taken to %#x\n", ac_pc + (imm << 2));
+  }
+  dbg_printf("Return = %#x\n", ac_pc + 4);
 };
 
 //!Instruction sys_call behavior method.
@@ -758,6 +892,25 @@ void ac_behavior( sys_call )
 //!Instruction instr_break behavior method.
 void ac_behavior( instr_break )
 {
-  fprintf(stderr, "instr_break behavior not implemented.\n"); 
+  fprintf(stderr, "instr_break behavior not implemented.\n");
   exit(EXIT_FAILURE);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
