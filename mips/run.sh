35d5ad86afcdf01a272fcb1137b6f7da0baa7aec
Ulisses Malta Santos <ulissesmaltasantos@gmail.com>
2015-04-17 09:29:06 -0300

    Add run.sh

---
+++ mips/run.sh
@@ -0,0 +1,5 @@
+export LD_LIBRARY_PATH=/home/staff/rodolfo/mc723/systemc/lib-linux64:$LD_LIBRARY_PATH
+export PATH=/home/staff/rodolfo/mc723/archc/bin:/home/staff/rodolfo/compilers/bin:$PATH
+
+./mips.x --load=./../MipsMibench/automotive/qsort/qsort_small
+
